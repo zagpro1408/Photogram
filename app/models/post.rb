931e799ca79fc60d63cfcdbaf41b528f22ae7d3e
zagpro1408 <zagorskybobuyc@gmail.com>
2017-04-21 11:29:35 +0300

    Begin using gem paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates :image, presence: true
+
+    # Редактирует размеры по ширине изобрадения на 640px
+    has_attached_file :image, styles: { :medium => "640x" }
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
